31917caf4f550b98a4fcb9333d72bf67673e431d
Artur Drabazha <jhorr2222@gmail.com>
2025-12-30 21:31:46 +0200

    Add debug

--- src/kui/_pyinstaller_hooks/__init__.py
+++ src/kui/_pyinstaller_hooks/__init__.py
@@ -2,4 +2,9 @@ import os
 
 
 def get_hook_dirs():
-    return [os.path.dirname(__file__)]
+    dirr = os.path.dirname(__file__)
+
+    with open("C:\\Users\\djara\\AppData\\Roaming\\SaveGem\\test.txt", "w") as file:
+        file.write(dirr)
+
+    return [dirr]
