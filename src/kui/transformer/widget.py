4a0ce2168dceb6c7a26158b30b3965a280ddf317
Artur Drabazha <jhorr2222@gmail.com>
2026-01-14 20:12:55 +0200

    Update metadata transformer object key names

--- src/kui/transformer/widget.py
+++ src/kui/transformer/widget.py
@@ -29,7 +29,7 @@ class JSONWidgetDataTransformer(ProviderDataTransformer):
 
         widgets = []
 
-        parent["section_id"] = root_widget.get("section_id")
+        parent["section"] = root_widget.get("section")
         children = parent.get("children", [])
 
         if "stylesheet" in parent:
@@ -44,7 +44,7 @@ class JSONWidgetDataTransformer(ProviderDataTransformer):
         for widget in children:
             order += 1
             widget["order_id"] = order
-            widget["parent_widget_id"] = parent["widget_id"]
+            widget["parent"] = parent["id"]
 
             for child_widget in self.__flatten_tree(root_widget, widget):
                 widgets.append(child_widget)
@@ -66,13 +66,13 @@ class JSONWidgetDataTransformer(ProviderDataTransformer):
         return data
 
     def __format_template_section(self, section_name: str, widget: dict, section: list[dict]):
-        section_id = f"{widget["widget_id"]}__template_{section_name}"
+        section_id = f"{widget["id"]}__template_{section_name}"
         order = 0
 
         for segment in section:
             order += 1
             segment["order_id"] = order
-            segment["section_id"] = section_id
+            segment["section"] = section_id
 
         return self.flatten(section)
 
@@ -85,9 +85,9 @@ class JSONWidgetDataTransformer(ProviderDataTransformer):
         widgets = []
 
         for widget in widget_data:
-            section_id = widget.get("section_id")
-            widget_id = widget.get("widget_id")
-            widget_parent_id = widget.get("parent_widget_id")
+            section_id = widget.get("section")
+            widget_id = widget.get("id")
+            widget_parent_id = widget.get("parent")
             widget_unique_id = f"{section_id}.{widget_id}"
             widget_unique_parent_id = f"{section_id}.{widget_parent_id}"
             events = widget.get("events", [])
@@ -132,17 +132,17 @@ class JSONWidgetDataTransformer(ProviderDataTransformer):
 
         for widget in data:
 
-            section_id = widget.get("section_id")
-            parent_widget_id = widget.get("parent_widget_id")
+            section_id = widget.get("section")
+            parent_widget_id = widget.get("parent")
             stylesheet = widget.get("stylesheet")
 
             # Only show section ID on root widgets.
             if parent_widget_id is not None and section_id is not None:
-                del widget["section_id"]
+                del widget["section"]
 
             # Remove parent widget id.
             if parent_widget_id is not None:
-                del widget["parent_widget_id"]
+                del widget["parent"]
 
             # Remove order id.
             if "order_id" in widget:
@@ -167,8 +167,8 @@ class JSONWidgetDataTransformer(ProviderDataTransformer):
         def link_template(segment: dict, target_widget_id: str, area_name: str, widgets: list[dict]):
 
             for widget in widgets:
-                if target_widget_id == widget.get("widget_id"):
-                    del segment["section_id"]
+                if target_widget_id == widget.get("id"):
+                    del segment["section"]
 
                     template = widget.get("template", {})
                     template_area = template.get(area_name, [])
@@ -179,10 +179,10 @@ class JSONWidgetDataTransformer(ProviderDataTransformer):
 
                 link_template(segment, target_widget_id, area_name, widget.get("children", []))
 
-        template_segments = [segment for segment in data if "__template" in segment.get("section_id")]
+        template_segments = [segment for segment in data if "__template" in segment.get("section")]
 
         for segment in template_segments:
-            section_id = segment.get("section_id")
+            section_id = segment.get("section")
             parent_widget_id, segment_code = section_id.split("__")
             segment_name = segment_code.split("_")[1]
 
